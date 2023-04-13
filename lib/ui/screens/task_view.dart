a2e3a6fe607d9f280e96ec4b1c01b5fa35b14297
fioresat <mekhonoshinama@gmail.com>
2023-04-13 22:24:58 +0200

    colors

--- lib/ui/screens/task_view.dart
+++ lib/ui/screens/task_view.dart
@@ -87,6 +87,9 @@ class _TaskViewState extends State<TaskView> {
           context.read<AppBloc>().add(
                 AppEventGoToMainView(listModel: widget.listModel),
               );
+          setState(() {
+            taskCurrentColorIndex = -1;
+          });
         },
         onClosePanelTap: () {
           context.read<AppBloc>().add(
